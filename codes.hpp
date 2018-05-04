9ee0eb3e7778c16fcaac6377ef028ba829362119
Przemek Szuplak <szuplak@gmail.com>
2018-05-04 23:15:55 +0200

    adding math and codes

---
+++ codes.hpp
@@ -0,0 +1,8 @@
+namespace my
+{
+enum : int
+       {
+	       ZERO = 0,
+	       ONE = 1
+       };
+}
---
+++ math.hpp
@@ -0,0 +1,10 @@
+#define PI 3.13
+
+namespace math
+{
+	struct Vector2D
+	{
+		float x;
+		float y;
+	};
+}
